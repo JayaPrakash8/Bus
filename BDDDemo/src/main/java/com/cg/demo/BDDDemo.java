5f4171a8b1262babd2a0e40d9c4661da60e84e41
javija <javija@DIN73003057.corp.capgemini.com>
2018-11-23 14:32:24 +0530

    commit

---
+++ BDDDemo/src/main/java/com/cg/demo/BDDDemo.java
@@ -0,0 +1,17 @@
+package com.cg.demo;
+
+public class BDDDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! Github");
+		
+		getMesssage();
+
+	}
+
+	private static void getMesssage() {
+		System.out.println("Hi!!!!");
+		
+	}
+
+}
